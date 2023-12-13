e0a7ac515c53cab2d37efc68bec479167d49f0d1
Ramkrishna Sharma <vishalbmx07@gmail.com>
2023-12-13 17:02:28 +0530

    Mul & Div added

--- src/main/java/Mypkg/MyCalculator.java
+++ src/main/java/Mypkg/MyCalculator.java
@@ -9,12 +9,23 @@ public class MyCalculator {
 	{
 		return a-b;	
 	}
+	public int mul(int a, int b) 
+	{
+		return a*b;	
+	}
+	public int div(int a, int b) 
+	{
+		return a/b;	
+	}
 
 	public static void main(String[] args) {
 
 		MyCalculator calc = new MyCalculator();
 		System.out.println("Sum is :"+calc.sum(20, 10));
 		System.out.println("Diff is :"+calc.diff(20, 10));
+		System.out.println("Mul is :"+calc.mul(20, 10));
+		System.out.println("Div is :"+calc.div(20, 10));
+		
 		
 
 	}
